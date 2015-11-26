6ff3c81238f7b63eb9811841515ce34c399e6a46
Karla <Karla@Karla-VAIO>
2015-11-26 14:14:52 +0100

    se añade imagenes al panel de navegacion

--- ComparatorTechnologyProject/src/comparatortechnologyproject/ApplicationWorkbenchWindowAdvisor.java
+++ ComparatorTechnologyProject/src/comparatortechnologyproject/ApplicationWorkbenchWindowAdvisor.java
@@ -22,6 +22,7 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
         configurer.setInitialSize(new Point(600, 400));
         configurer.setShowCoolBar(true);
         configurer.setShowStatusLine(false);
+        configurer.setTitle("Comparador de Tecnologías");
     }
     
 }
--- ComparatorTechnologyProject/src/comparatortechnologyproject/ViewLabelProvider.java
+++ ComparatorTechnologyProject/src/comparatortechnologyproject/ViewLabelProvider.java
@@ -1,19 +1,25 @@
 package comparatortechnologyproject;
 
+import java.net.URL;
+
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
-import org.eclipse.ui.ISharedImages;
-import org.eclipse.ui.PlatformUI;
 
 class ViewLabelProvider extends LabelProvider {
 
 	public String getText(Object obj) {
 		return obj.toString();
 	}
+	@SuppressWarnings("deprecation")
 	public Image getImage(Object obj) {
-		String imageKey = ISharedImages.IMG_OBJ_ELEMENT;
-		if (obj instanceof TreeParent)
-		   imageKey = ISharedImages.IMG_OBJ_FOLDER;
-		return PlatformUI.getWorkbench().getSharedImages().getImage(imageKey);
+		URL url = Activator.getDefault().find(new Path("icons/subscriberStation.gif"));
+		if (obj instanceof TreeParent){
+			url = Activator.getDefault().find(new Path("icons/baseStation.gif"));
+		}
+		
+		ImageDescriptor id = ImageDescriptor.createFromURL(url);
+		return id.createImage();
 	}
 }
